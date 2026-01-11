5ca6b5423b135f583dcb59c78ffa78f02e646025
Gabby <gabbyvbyers@gmail.com>
2026-01-10 23:34:48 -0800

    Hello World w/ rand() in C

--- prog1.c
+++ prog1.c
@@ -1,11 +1,24 @@
-#include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+	srand(time(NULL));
+	int myrand = rand();
+	myrand = myrand % 11;
+
+	if (myrand <= 4) {
+		printf("\nEat more beef, kick less cats\n");
+	}
+
+	else if (myrand <= 9) {
+		printf("\nFRODO LIVES\n");
+	}
+
+	else {
+		printf("\nLarn is the best roguelike\n");
+	}
+
+	printf("The random number was: %d\n\n", myrand);
+	return 0;
 }
